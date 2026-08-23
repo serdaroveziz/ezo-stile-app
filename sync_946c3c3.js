c6d1bab5fc35840c81bc72b4389836c00164e7fe
serdaroveziz <serdaroveziz@gmail.com>
2026-08-23 23:50:53 +0300

    fix(deploy): Clean sync_946c3c3.js

--- sync_946c3c3.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const commit = '946c3c3';
-
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replaceAll(/Build:\s*[a-f0-9]{7}/gi, `Build: ${commit}`);
-fs.writeFileSync('index.html', html, 'utf8');
-
-console.log(`Set Build: ${commit} in index.html`);
